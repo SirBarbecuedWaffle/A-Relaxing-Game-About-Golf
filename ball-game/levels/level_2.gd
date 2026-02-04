0759a2f4775e7123af0e23eb5b18dcd44458edc5
SirBarbecuedWaffle <barbecuedwaffle101@gmail.com>
2026-02-04 13:12:18 -0600

    level 1 finished and level 2 working

--- ball-game/levels/level_2.gd
+++ ball-game/levels/level_2.gd
@@ -1,5 +1,6 @@
-extends StaticBody3D
-@onready var success_particles: GPUParticles3D = $CSGCombiner3D/SuccessParticles
+extends Node3D
+@onready var reset_point: Marker3D = $resetPoint
+@onready var player: Player = $Player
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,7 +13,7 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_area_3d_body_entered(body: Node3D) -> void:
-	success_particles.emitting=true
+func _on_reset_area_body_entered(body: Node3D) -> void:
 	await get_tree().create_timer(3.0).timeout
-	get_tree().change_scene_to_file("res://levels/level.tscn")
+	player.global_position=reset_point.global_position
+	player.linear_velocity=Vector3.ZERO
