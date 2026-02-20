2bf32212d3c1a7fcc9e0378fc8112d146f1d15ae
SirBarbecuedWaffle <barbecuedwaffle101@gmail.com>
2026-02-20 13:21:45 -0600

    Level 7 work done

--- ball-game/levels/moving_obstacle.gd
+++ ball-game/levels/moving_obstacle.gd
@@ -2,7 +2,7 @@ extends RigidBody3D
 @onready var marker_3d: Marker3D = $Marker3D
 @onready var marker_3d_2: Marker3D = $Marker3D2
 @onready var csg_box_3d: CSGBox3D = $CSGBox3D
-
+@export var time:=1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,10 +16,10 @@ func _ready() -> void:
 func move() -> void:
 	var clubtween = create_tween()
 	clubtween.set_trans(Tween.TRANS_SINE)
-	clubtween.tween_property(csg_box_3d,"global_position",marker_3d.global_position,1)    
+	clubtween.tween_property(csg_box_3d,"global_position",marker_3d.global_position,time)    
 	await clubtween.finished
 	var clubtween2= create_tween()
 	clubtween2.set_trans(Tween.TRANS_SINE)
-	clubtween2.tween_property(csg_box_3d,"global_position",marker_3d_2.global_position,1)    
+	clubtween2.tween_property(csg_box_3d,"global_position",marker_3d_2.global_position,time)    
 	await clubtween2.finished
 	move()
